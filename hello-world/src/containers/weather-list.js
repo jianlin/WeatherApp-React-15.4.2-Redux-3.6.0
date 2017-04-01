617a1a7c57ece8cd254956a9c1e27a7611085d3f
Kin <winterheat@gmail.com>
2017-04-01 16:47:46 -0700

    clean up

--- hello-world/src/containers/weather-list.js
+++ hello-world/src/containers/weather-list.js
@@ -1,53 +1,26 @@
 
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import {countries} from "country-data";
 import CountryData from 'country-data'
 
-console.log("CountryData.countries", CountryData.countries);
-// const CountryData2 = require(CountryData)
 class WeatherList extends Component {
 
   weatherDetails() {
-    // console.log("countries", CountryData.countries, CountryData.countries["US"])
-    // console.log(JSON.stringify(countries['US']));
-    // console.log(JSON.stringify(CountryData.countries['US']));
-    // console.log(JSON.stringify(CountryData.countries.TW));
-    console.log("CountryData.countries.TW", CountryData.countries.TW);
-    // console.log(countries.TW);
 
     function convertKToC(k) {
-      return Math.round(10*(k - 273.15)) / 10
+      return Math.round(10 * (k - 273.15)) / 10;
     }
-    //console.log("countries", CountryData, CountryData.countries, CountryData.countries.all, CountryData.countries["TW"])
+
     return this.props.weatherList.map((weather) => {
-
-      // const temps = weather.list.map((data) => Math.round(10*(data.main.temp - 273.15)) / 10);
-      // const humidities = weather.list.map((data) => data.main.humidity);
-      // const pressures = weather.list.map((data) => data.main.pressure);
-
-      // console.log("temps", temps);
 
       return (
         <tr key={weather.name + "," + weather.sys.country}>
           <td>{ weather.name }</td>
           <td>{ CountryData.countries[weather.sys.country].name }</td>
-{
-          //  <td>{ CountryData.countries[weather.city.country].emoji }</td>
-}
-
-          {/* }<td> {CountryData.countries[weather.city.country]}</td> */}
           <td>{ convertKToC(weather.main.temp) } °C</td>
           <td> {convertKToC(weather.main.temp_min)} - {convertKToC(weather.main.temp_max)} °C</td>
-
           <td>{ weather.main.humidity } %</td>
-
           <td>{ weather.weather[0].description }</td>
-          {
-          // <td><Chart data={temps} color="orange" low={Math.min.apply(null, temps)} high={Math.max.apply(null, temps)} unit="°C" /></td>
-          // <td><Chart data={humidities} color="blue" low={Math.min.apply(null, humidities)} high={Math.max.apply(null, humidities)} unit="%" /></td>
-          // <td><Chart data={pressures} color="green" low={Math.min.apply(null, pressures)} high={Math.max.apply(null, pressures)} unit="hPa" /></td>
-        }
         </tr>
       )
 
@@ -56,37 +29,34 @@ class WeatherList extends Component {
 
   render() {
     if (this.props.weatherList.length === 0) {
-      return <div></div>;
+      return (
+        <div></div>
+      );
     } else {
       return (
         <table className="table table-hover weather-info-list">
-        <thead>
-        <tr>
-        <th>City</th>
-        <th>Country</th>
-        <th>Temperature</th>
-        <th>Temperature Range</th>
-        <th>Humidity</th>
-        <th>Conditions</th>
-        </tr>
 
-        {/*}<th>Country</th>
-        <th>Temperature</th>
-        <th>Description</th>
-        <th>Description</th>
-        <th>Description</th>
-        <th>Description</th>*/}
-        </thead>
-        <tbody>
-          {this.weatherDetails()}
-        </tbody>
+          <thead>
+            <tr>
+              <th>City</th>
+              <th>Country</th>
+              <th>Temperature</th>
+              <th>Temperature Range</th>
+              <th>Humidity</th>
+              <th>Conditions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.weatherDetails()}
+          </tbody>
+
         </table>
       );
     }
 
   }
 }
-
 
 
 function mapStateToProps(state) {    // HOW TO CONNECT APP STATE TO CONTAINER: map the app level state to props
