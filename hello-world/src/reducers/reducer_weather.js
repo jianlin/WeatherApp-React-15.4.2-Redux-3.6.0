5be1a892ab6e414e6beb47fc36fd08ddcd68eb3d
Kin <winterheat@gmail.com>
2017-04-01 18:20:05 -0700

    handle ajax data error

--- hello-world/src/reducers/reducer_weather.js
+++ hello-world/src/reducers/reducer_weather.js
@@ -2,6 +2,10 @@ import { FETCH_WEATHER } from "../actions/index";
 
 export default function(state = [], action) {
 
+  if (action.error) {
+    return state;
+  }
+  
   switch(action.type) {
     case FETCH_WEATHER:
       return [action.payload.data].concat(state);
